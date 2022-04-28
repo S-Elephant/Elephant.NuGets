97d1034d52439a2798f74d25c7b33e0d25abc54a
SquirtingElephant <75314121+SquirtingElephant@users.noreply.github.com>
2022-04-28 15:22:31 +0200

    DbSetExtensions

---
+++ Elephant.Database/DbSetExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Elephant.Database
+{
+	/// <summary>
+	/// Provides extension methods for <see cref="DbSet{TEntity}"/>.
+	/// </summary>
+	public static class DbSetExtensions
+    {
+		/// <summary>
+		/// Clear the specified <see cref="DbSet{TEntity}"/>.
+		/// </summary>
+		/// <returns>The <paramref name="dbSet"/>.</returns>
+		/// <remarks>Does not reset the auto increment seed.</remarks>
+		public static DbSet<TEntity> Clear<TEntity>(this DbSet<TEntity> dbSet)
+			where TEntity : class
+		{
+			foreach (TEntity entity in dbSet)
+			{
+				dbSet.Remove(entity);
+			}
+
+			return dbSet;
+		}
+	}
+}
