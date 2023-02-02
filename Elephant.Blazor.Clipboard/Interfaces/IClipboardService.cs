f4f155b45577315e0ab8e5b6d4a36a0eac709c1e
SquirtingElephant <75314121+SquirtingElephant@users.noreply.github.com>
2023-02-02 18:08:02 +0100

    New project: Elephant.Blazor.Clipboard

---
+++ Elephant.Blazor.Clipboard/Interfaces/IClipboardService.cs
@@ -0,0 +1,18 @@
+﻿namespace Elephant.Blazor.Clipboard.Interfaces
+{
+	/// <summary>
+	/// Clipboard service (frontend).
+	/// </summary>
+	public interface IClipboardService
+	{
+		/// <summary>
+		/// Get text.
+		/// </summary>
+		ValueTask<string> GetTextAsync();
+
+		/// <summary>
+		/// Set text.
+		/// </summary>
+		ValueTask SetTextAsync(string text);
+	}
+}
